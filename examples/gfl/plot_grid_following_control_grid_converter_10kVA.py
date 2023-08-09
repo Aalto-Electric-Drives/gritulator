9310bc4277c72c78a65d6563a94936700ce5a88d
Rayane Mourouvin <rayane.mourouvin@aalto.fi>
2023-08-09 18:04:16 +0300

    correct examples docstrings to fit with documentation

--- examples/gfl/plot_grid_following_control_grid_converter_10kVA.py
+++ examples/gfl/plot_grid_following_control_grid_converter_10kVA.py
@@ -1,15 +1,17 @@
 """
-Example simulation script: 10-kVA grid converter connected to a symmetrical
-three-phase AC voltage source (grid) through an inductive filter.
+10-kVA grid following converter, power control
+==============================================
     
-The control system includes
-    - Phase-Locked Loop (PLL) to synchronize with the grid;
-    - Current reference generation;
-    - Proportional-integral (PI) vector current controller
+This example simulates a grid following controlled converter connected to a
+strong grid. The control system includes a phase-locked loop (PLL) to
+synchronize with the grid, a current reference generatior and a PI-based
+current controller.
+
 """
 
 # %%
-# Import the packages.
+# Imports.
+
 import numpy as np
 from motulator import model, control
 from motulator import BaseValuesElectrical, plot_grid
@@ -20,19 +22,26 @@ start_time = time.time()
 
 # %%
 # Compute base values based on the nominal values (just for figures).
+
 base_values = BaseValuesElectrical(
     U_nom=400, I_nom=14.5, f_nom=50.0, P_nom=10e3)
 
 
 # %%
-# Configure the system model
+# Create the system model.
+
+# grid impedance and filter model
 grid_filter = model.LFilter(L_f=10e-3, L_g=0, R_g=0)
+# AC grid model (either constant frequency or dynamic electromechanical model)
 grid_model = model.StiffSource(w_N=2*np.pi*50)
 converter = model.Inverter(u_dc=650)
-
 mdl = model.ac_grid.StiffSourceAndLFilterModel(
     grid_filter, grid_model, converter)
 
+# %%
+# Configure the control system.
+
+# Control parameters
 pars = control.gfl.GridFollowingCtrlPars(
             L_f=10e-3,
             R_f=0,
@@ -44,9 +53,9 @@ ctrl = control.gfl.GridFollowingCtrl(pars)
 
 
 # %%
+# Set the time-dependent reference and disturbance signals.
 
-# Set the active and reactive power references
-# that are inputs to the control system
+# Set the active and reactive power references.
 ctrl.p_g_ref = lambda t: (t > .02)*(5e3)
 ctrl.q_g_ref = lambda t: (t > .04)*(4e3)
 
@@ -54,12 +63,16 @@ ctrl.q_g_ref = lambda t: (t > .04)*(4e3)
 e_g_abs_var =  lambda t: np.sqrt(2/3)*400
 mdl.grid_model.e_g_abs = e_g_abs_var # grid voltage magnitude
 
+# %%
 # Create the simulation object and simulate it
+
 sim = model.Simulation(mdl, ctrl, pwm=False)
 sim.simulate(t_stop = .1)
 
 # Print the execution time
 print('\nExecution time: {:.2f} s'.format((time.time() - start_time)))
 
+# %%
 # Plot results in SI or per unit values
+
 plot_grid(sim, base=base_values,plot_pcc_voltage=True)
